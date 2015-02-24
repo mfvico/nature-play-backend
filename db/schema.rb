b95f07281b16df83414eb7081746b6da72f81d42
mrjadaml <mrjadaml@gmail.com>
2015-02-24 11:15:23 -0700

    changed table name from locations to parks. removed turbolinks

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218213546) do
+ActiveRecord::Schema.define(version: 20150224180437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "locations", force: :cascade do |t|
+  create_table "parks", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "city"
