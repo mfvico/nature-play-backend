26e0154c3a0fec23034be5cf821e526c7966a620
Mello <dan.mello1001@gmail.com>
2015-02-25 15:09:55 -0700

    sending corect data out as JSON

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,8 @@
+class ParksController < ApplicationController
+
+  def index
+    @parks = Park.all
+    render json: @parks
+  end
+
+end
--- app/serializers/parks_serialzer.rb
+++ app/serializers/parks_serialzer.rb
@@ -1,3 +1,3 @@
 class ParksSerializer < ActiveModel::Serializer
-  attributes :name, :address, :city, :state, :zip, :lat, :lng, :body, :amenities, :image
+  attributes :name, :address, :city, :state, :zip, :latitude, :longitude, :body, :amenities, :image
 end
